844a27f9e3f6b9118ecce2418b118a387c9aa388
strix <damjan.oslaj@gmail.com>
2018-06-19 20:09:53 +0200

    .
    .

--- djangoproject/posts/urls.py
+++ djangoproject/posts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    path('details/<int:id>/', views.details, name='details'),
 ]
+
--- djangoproject/posts/views.py
+++ djangoproject/posts/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Posts
+
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html', {
-        'title': 'Latest posts2'
-    })
+
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title': 'Latest posts2',
+        'posts': posts
+    }
+
+    return render(request, 'posts/index.html', context)
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'posts/details.html', context)
